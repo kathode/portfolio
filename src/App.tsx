d4e08f5b19c6b82ff74ef5abc54fa27485d44c85
Kathode <dtu@deakin.edu.au>
2023-12-30 17:54:25 +1100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import "mapbox-gl/dist/mapbox-gl.css";
+import mapboxgl from "mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
+
+mapboxgl.accessToken = "pk.eyJ1Ijoia2F0aG9kZSIsImEiOiJjbHFyZm4weXU0bDR6MmtucGV3YXNoeTg3In0.27-IO2S4uvWz4Lu2RkOhlA";
+
+function App() {
+  return <>test</>;
+}
+
+export default App;
