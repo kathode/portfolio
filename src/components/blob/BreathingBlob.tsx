2fdeca7bc8a5e44a247e903497f3f290b5a3ef59
Kathode <dtu@deakin.edu.au>
2024-01-02 01:44:37 +1100

    add component to loop through the blobs smoothly using framer

---
+++ src/components/blob/BreathingBlob.tsx
@@ -0,0 +1,45 @@
+import { motion } from "framer-motion";
+import { blobs1, blob2, blob3, blob4, blob5, blob6 } from "./blobs";
+
+type Props = {
+  size: string;
+  fill?: string;
+  duration?: number;
+  hover?: boolean;
+};
+
+const BreathingBlob = (props: Props) => {
+  const { size, fill = "#2a363b", duration = 20, hover = false } = props;
+
+  return (
+    <svg
+      className="logo"
+      viewBox="0 0 900 900"
+      width={size}
+      height={size}
+      style={{
+        ...(hover && {
+          filter: "drop-shadow(0 0 3em #ffb4af)",
+        }),
+      }}
+    >
+      <g transform="translate(454.81072117472434 482.63282018887986)">
+        <motion.path
+          fill={fill}
+          d={blob2}
+          animate={{
+            d: [blobs1, blob2, blob3, blob4, blob5, blob6, blobs1],
+          }}
+          transition={{
+            duration: duration,
+            ease: "easeInOut",
+            yoyo: Infinity,
+            repeat: Infinity,
+          }}
+        />
+      </g>
+    </svg>
+  );
+};
+
+export default BreathingBlob;
