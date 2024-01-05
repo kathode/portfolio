b806ebd6a91649a8db3af58768206f09e07bcfbe
Kathode <dtu@deakin.edu.au>
2024-01-06 01:40:20 +1100

    made mapbox responsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import "./App.scss";
 import Logo from "./components/logo/Logo";
 import Mapbox from "./components/map/Mapbox";
+import "./components/map/Mapbox.scss";
 
 function App() {
   return (
     <div className="App">
       <Logo />
       <div className="h-[50svh] bg-[#151b1d] flex justify-center">
-        <div className="w-1/2">
+        <div className="mapbox-container">
           <Mapbox />
         </div>
       </div>
