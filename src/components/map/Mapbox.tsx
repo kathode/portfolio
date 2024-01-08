5a6c4390c74d19923a02ae24f999dff4b068b0d1
Kathode <dtu@deakin.edu.au>
2024-01-08 22:00:59 +1100

    -removed settimeout from mapbox - planned to set the delay in the parent

--- src/components/map/Mapbox.tsx
+++ src/components/map/Mapbox.tsx
@@ -12,30 +12,28 @@ const Mapbox = () => {
   const [zoom, setZoom] = useState(11);
 
   useEffect(() => {
-    setTimeout(() => {
-      if (map.current) return; // initialize map only once
+    if (map.current) return; // initialize map only once
 
-      map.current = new mapboxgl.Map({
-        container: mapContainer.current,
-        style: "mapbox://styles/kathode/clqz32u2800fh01rj8p486vmt",
-        center: [lng, lat],
-        zoom: zoom,
-        minZoom: 9,
-        maxZoom: 15,
-        pitch: 50,
-        bearing: 0,
-        maxBounds: [
-          [144.86, -37.98], // Southwest coordinates (longitude, latitude) bottom left
-          [145.25, -37.68], // Northeast coordinates (longitude, latitude) top right
-        ],
-      });
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: "mapbox://styles/kathode/clqz32u2800fh01rj8p486vmt",
+      center: [lng, lat],
+      zoom: zoom,
+      minZoom: 9,
+      maxZoom: 15,
+      pitch: 50,
+      bearing: 0,
+      maxBounds: [
+        [144.86, -37.98], // Southwest coordinates (longitude, latitude) bottom left
+        [145.25, -37.68], // Northeast coordinates (longitude, latitude) top right
+      ],
+    });
 
-      map.current.on("move", () => {
-        setLng(map.current.getCenter().lng.toFixed(4));
-        setLat(map.current.getCenter().lat.toFixed(4));
-        setZoom(map.current.getZoom().toFixed(2));
-      });
-    }, 1000);
+    map.current.on("move", () => {
+      setLng(map.current.getCenter().lng.toFixed(4));
+      setLat(map.current.getCenter().lat.toFixed(4));
+      setZoom(map.current.getZoom().toFixed(2));
+    });
   });
 
   return <div ref={mapContainer} className="map-container h-[100%] w-[100%] rounded-xl" />;
