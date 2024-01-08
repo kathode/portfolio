ea0f26716af721babf73954ea669eb67d64753fe
Kathode <dtu@deakin.edu.au>
2024-01-08 23:53:23 +1100

    -refactor/optimise code -pause rotation when user hovers over svg

--- src/components/svg/SvgHub.tsx
+++ src/components/svg/SvgHub.tsx
@@ -1,24 +1,28 @@
+import { useState } from "react";
 import * as SVG from "../svg/index.ts";
 import "./SvgHub.scss";
 
+type SvgComponents = { [key: string]: React.ComponentType<React.SVGProps<SVGSVGElement>> };
+const SvgComponents: SvgComponents = SVG;
+const svgKeys = Object.keys(SvgComponents);
+
 const SvgHub = () => {
+  const [hover, setHover] = useState(false);
+
+  const pause = () => {
+    setHover(true);
+  };
+
+  const play = () => {
+    setHover(false);
+  };
+
   return (
-    <div className="circle-container">
-      <SVG.React />
-      <SVG.Redux />
-      <SVG.Html />
-      <SVG.Css />
-      <SVG.Javascript />
-      <SVG.Typescript />
-      <SVG.Scss />
-      <SVG.Bootstrap />
-      <SVG.Tailwind />
-      <SVG.Github />
-      <SVG.Python />
-      <SVG.Figma />
-      <SVG.Threejs />
-      <SVG.Postman />
-      <SVG.Swagger />
+    <div className={`${hover ? "circle-container paused" : "circle-container"}`} style={{ animationPlayState: hover ? "paused" : "" }}>
+      {svgKeys.map((svgKey) => {
+        const SvgComponent = SvgComponents[svgKey];
+        return <SvgComponent key={svgKey} onMouseEnter={pause} onMouseLeave={play} />;
+      })}
     </div>
   );
 };
